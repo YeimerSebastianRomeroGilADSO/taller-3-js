520ebdd28438f633f17b3185f2c04968d10c7ed7
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-12-07 11:51:11 -0500

    punto 2 js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 1. Determinar si un número es positivo y menor que 100. 
-    let num = Number(prompt('Ingrese el numero'))
-    if (num<0) {
-        console.log('El numero tiene que ser positivo');
-    }
-    else if (num>=100) {
-        console.log(`El numero ${num} es positivo mayor que 100`);
-    } else {
-        console.log(`el numero ${num} es positivo menor que 100`);
-    }
+
+ // 2. Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
+ let num1 = Number(prompt(`Ingrese el numero `))
+ let num2 = Number(prompt(`Ingrese el numero `))
+ let num3 = Number(prompt(`Ingrese el numero `))
+ if (num1>num2 && num1>num3) {
+     console.log(`El numero ${num1} es mayor que el numero ${num2} y ${num3}`);
+ }
+ else if (num1>=100) {
+     console.log(`El numero ${num1} es positivo mayor que 100`);
+ }
+else if (num2>num1 && num2>num3){
+     console.log(`El numero ${num2} es mayor que el numero ${num1} y ${num3}`);
+ }
+ else if (num3>num1 && num3>num2) {
+     console.log(`El numero ${num3} es mayor que el numero ${num1} y ${num2}`);
+     }
+     else{
+         console.log('no se pudo encontrar numero mayor');
+     }
 })
