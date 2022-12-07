bbce554c61b1dae70afaef673ecc9ebf70cd206d
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-12-07 12:13:29 -0500

    punto 6 js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // 5. Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean positivos:
-    // área triangulo= lado * lado.
-    let base = Number(prompt('Ingrese la base  del rectangulo '))
-    let altura = Number(prompt('Ingrese la altura del rectangulo'))
-    if (altura<0 || base<0){
+  // 6. Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre
+    // en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es
+    // mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
+    let temperatura = Number(prompt('Ingrese el valor de la temperatura '))
+    let presion = Number(prompt('Ingrese el valor de la presion'))
+    if (presion>200) {
+        console.log('¡ALARMA LA PRESION ESTA DEMASIADA ALTA!');
     }
-    else{
-            area=base*altura
-            console.log(`El area del rectangulo es ${area}`);
-        }
-    
-        console.log('Solo se adminten valores positivos');
+    else if (temperatura>100){
+        console.log(`ALARMA LA TEMPERATURA ESTA DEMASIADO ALTA`);
+    }
+    else {
+        console.log('Los valores de temperatura y presion estan normales');
+    }
  })
